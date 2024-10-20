2cb69f43290aadaf9519aad6eb03894e8939f98d
temp-username-sg <temp.trunnghieu@gmail.com>
2024-10-20 19:13:57 +0700

    First commit

---
+++ js/myJs.js
@@ -0,0 +1,251 @@
+// Import Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  set,
+} from "https://www.gstatic.com/firebasejs/9.6.7/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJH6w9z7CK77HVPZD-n02-x_Lc10XP7zg",
+  authDomain: "lime-641d8.firebaseapp.com",
+  databaseURL: "https://lime-641d8-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "lime-641d8",
+  storageBucket: "lime-641d8.appspot.com",
+  messagingSenderId: "63315151560",
+  appId: "1:63315151560:web:f4c8d09eba1fedaa3a73fd"
+};
+
+// Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+function writePost(content, dateCreated) {
+  set(ref(db, "posts/" + dateCreated), {
+    content: content,
+    dateCreated: getTime(),
+  });
+}
+
+function writeYesClickTime(dateTime) {
+  set(ref(db, "yesClick/" + dateTime), {
+    dateTime: getTime(),
+  });
+}
+
+function writeVisitTime(dateTime) {
+  set(ref(db, "visits/" + dateTime), {
+    dateTime: getTime(),
+  });
+}
+
+function getTime() {
+  // Create a new Date object to get the current time
+  const date = new Date();
+
+  // Get the time zone offset in minutes
+  const timeZoneOffsetMinutes = date.getTimezoneOffset();
+
+  // Convert the offset to hours and calculate the sign
+  const offsetHours = Math.abs(Math.floor(timeZoneOffsetMinutes / 60));
+  const offsetSign = timeZoneOffsetMinutes > 0 ? "-" : "+";
+
+  // Format the date to the desired format with the UTC/GMT offset
+  const formattedDate = `${date.getFullYear()}/${String(
+    date.getMonth() + 1
+  ).padStart(2, "0")}/${String(date.getDate()).padStart(2, "0")} ${String(
+    date.getHours()
+  ).padStart(2, "0")}:${String(date.getMinutes()).padStart(2, "0")}:${String(
+    date.getSeconds()
+  ).padStart(2, "0")} UTC${offsetSign}${String(offsetHours).padStart(
+    2,
+    "0"
+  )}:${String(Math.abs(timeZoneOffsetMinutes) % 60).padStart(2, "0")}`;
+  return formattedDate;
+}
+
+// Main
+const textConfig = {
+  text1: "Một nụ cười luôn hé, thế giới vẫn quay~ 🍀🍀🍀",
+  text3: "Lime, take my energy ༼ つ ◕_◕ ༽つ",
+  text4: "Dù chuyện đôi mình chẳng thành, em mong chị đừng quên rằng bản thân chị tuyệt vời như thế nào. Nhờ có chị, suốt 5 năm ròng qua, em mới thật sự được tận hưởng cuộc sống ở Sài Gòn. Cảm ơn chị vì đã đến bên đời, người yêu đầu tiên và duy nhất ở Sài Gòn của em, Chanh biết nói.",
+  text5: "Chọn khóc",
+  text6: "Chọn cười",
+  text7: "Nếu mọi thứ có khó khăn quá, và chị cần một người bạn thật sự lắng nghe chị, thì em ở đây",
+  text8: "Gửi em",
+  text9: "Nothing",
+  text10: "Một vài điều em muốn nói",
+  text11:
+  "Chị xinh nhất lúc cười đấy, đừng quên điều đó nhé",
+  text12: "Đến trang của một người xinh đẹp",
+};
+
+$(document).ready(function () {
+  // process bar
+  setTimeout(function () {
+    firstQuestion();
+    $(".spinner").fadeOut();
+    $("#preloader").delay(350).fadeOut("slow");
+    $("body").delay(350).css({
+      overflow: "visible",
+    });
+  }, 600);
+
+  $("#text3").html(textConfig.text3);
+  $("#text4").html(textConfig.text4);
+  $("#no").html(textConfig.text5);
+  $("#yes").html(textConfig.text6);
+
+  function firstQuestion() {
+    $(".content").hide();
+    Swal.fire({
+      title: textConfig.text1,
+      // text: textConfig.text2,
+      imageUrl: "img/main_pic.jpg",
+      imageWidth: 220,
+      showCancelButton: false,
+      confirmButtonColor: "#fe8a71",
+      confirmButtonText: "Tiếp tục",
+      background: 'url("img/input-bg.jpg")',
+      imageAlt: "Custom image",
+    }).then(function () {
+      $(".content").show(350);
+      playSound();
+      writeVisitTime(new Date().getTime());
+    });
+  }
+
+  function playSound() {
+    var audio = new Audio("sound/music.mp3");
+    audio.volume = 0.4;
+    audio.play();
+    if (typeof audio.loop == "boolean") {
+      audio.loop = true;
+    } else {
+      audio.addEventListener(
+        "ended",
+        function () {
+          this.currentTime = 0;
+          this.play();
+        },
+        false
+      );
+    }
+  }
+
+  // switch button position
+  function switchButton() {
+    // var audio = new Audio("sound/duck.mp3");
+    // audio.play();
+    var leftNo = $("#no").css("left");
+    var topNO = $("#no").css("top");
+    var leftY = $("#yes").css("left");
+    var topY = $("#yes").css("top");
+    $("#no").css("left", leftY);
+    $("#no").css("top", topY);
+    $("#yes").css("left", leftNo);
+    $("#yes").css("top", topNO);
+  }
+  // move random button position
+  function moveButton() {
+    // var audio = new Audio("sound/Swish1.mp3");
+    // audio.play();
+    if (screen.width <= 600) {
+      var x = Math.random() * 300;
+      var y = Math.random() * 500;
+    } else {
+      var x = Math.random() * 500;
+      var y = Math.random() * 500;
+    }
+    var left = x + "px";
+    var top = y + "px";
+    $("#no").css("left", left);
+    $("#no").css("top", top);
+  }
+
+  var n = 0;
+  $("#no").mousemove(function () {
+    if (n < 1) switchButton();
+    if (n > 1) moveButton();
+    n++;
+  });
+  $("#no").click(() => {
+    if (screen.width >= 900) switchButton();
+  });
+
+  // generate text in input
+  function textGenerate() {
+    var n = "";
+    var text = " " + textConfig.text9;
+    var a = Array.from(text);
+    var textVal = $("#txtReason").val() ? $("#txtReason").val() : "";
+    var count = textVal.length;
+    if (count > 0) {
+      for (let i = 1; i <= count; i++) {
+        n = n + a[i];
+        if (i == text.length + 1) {
+          $("#txtReason").val("");
+          n = "";
+          break;
+        }
+      }
+    }
+    $("#txtReason").val(n);
+  }
+
+  // show popup
+  $("#yes").click(function () {
+    // var audio = new Audio("sound/tick.mp3");
+    // audio.play();
+    Swal.fire({
+      title: textConfig.text7,
+      // html: true,
+      input: "text",
+      width: 900,
+      padding: "2em",
+      // html: "<input type='text' class='form-control' style='height:100px' id='txtReason'  placeholder=' '>",
+      background: '#FFFFFF',
+      // backdrop: `
+      //               rgba(0,0,123,0.4)
+      //               url("img/giphy2.gif")
+      //               left top
+      //               no-repeat
+      //             `,
+      showCancelButton: false,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonColor: "#fe8a71",
+      cancelButtonColor: "#f6cd61",
+      confirmButtonText: textConfig.text8,
+      customClass: "swal-image-yes",
+    }).then((result) => {
+      if (result.value) {
+        var time = new Date().getTime();
+        // writeYesClickTime(time)
+        if (result.value != null && result.value != "") {
+          writePost(result.value, time);
+        }
+        Swal.fire({
+          width: "90%",
+          confirmButtonText: textConfig.text12,
+          background: "#F8F8F8",
+          title: textConfig.text10,
+          text: textConfig.text11,
+          confirmButtonColor: "#83d0c9",
+          onClose: () => {
+            window.location = "https://www.instagram.com/liliilalely";
+          },
+        });
+      }
+    });
+
+    // $("#txtReason").focus(function () {
+    //   var handleWriteText = setInterval(function () {
+    //     textGenerate();
+    //   }, 10);
+    //   $("#txtReason").blur(function () {
+    //     clearInterval(handleWriteText);
+    //   });
+    // });
+  });
+});
